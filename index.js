f1f7fa3d1badeed52f7d4ce1b021492d797ed167
maksym davydenko <Maksym_Davydenko@epam.com>
2020-07-31 12:30:33 +0300

    change camera logic

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const menuBtn = document.querySelector('.menu');
 const menuLine = document.querySelector('.menu__line');
 const menuList = document.querySelector('.header__nav');
+const disapleLine = document.querySelector('.disable-line');
 
 const wrapContent = document.querySelector('.wrap-content');
 const bgBtn = document.querySelector('#bgBtn');
-const disapleLine = document.querySelector('.disable-line');
-
-(async () => {
-    try {
-        await navigator.mediaDevices.getUserMedia({ video: true });
-        console.log("GOT CAM");
-    } catch (e) {
-        console.log("GOT ERROR : " + e);
-        disapleLine.style.display = 'block';
-        wrapContent.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-
-        bgBtn.setAttribute("disabled", "disabled");
-
-        alert('you camera is disabled of your device not support :(')
-    }
-})();
 
 menuBtn.addEventListener('click', () => {
     menuLine.classList.toggle('active');
@@ -27,9 +12,7 @@ menuBtn.addEventListener('click', () => {
         '0' : '180px';
 });
 
-
 bgBtn.addEventListener('click', () => {
-
     if (wrapContent.style.backgroundColor !== 'rgb(0, 0, 0)') {
         wrapContent.style.backgroundColor = 'rgba(0, 0, 0, 1)'
         disapleLine.style.display = 'block';
@@ -41,5 +24,3 @@ bgBtn.addEventListener('click', () => {
     }
 
 })
-
-
