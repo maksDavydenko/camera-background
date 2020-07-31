d0086acc66d9454faea70dffe13f5506e24fbc78
maksym davydenko <Maksym_Davydenko@epam.com>
2020-07-31 13:00:22 +0300

    fix code

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ const disapleLine = document.querySelector('.disable-line');
 const wrapContent = document.querySelector('.wrap-content');
 const bgBtn = document.querySelector('#bgBtn');
 
-(async () => {
-    try {
-        await navigator.mediaDevices.getUserMedia({ video: true });
-        console.log("GOT CAM");
-    } catch (e) {
-        console.log("GOT ERROR : " + e);
-        disapleLine.style.display = 'block';
-        wrapContent.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-
-        bgBtn.setAttribute("disabled", "disabled");
-
-        alert('you camera is disabled of your device not support :(')
-    }
-})();
-
 menuBtn.addEventListener('click', () => {
     menuLine.classList.toggle('active');
     menuList.style.height = menuList.style.height === '180px' ?
